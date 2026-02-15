95e7418c1d83a98a9cc574b94176f5a054d9b578
vinodnaikr <142738355+vinodnaikr@users.noreply.github.com>
2026-02-15 16:38:13 +0530

    Replace train_model with compile_and_train function

--- trainig.py
+++ trainig.py
@@ -1,3 +1,18 @@
-# src/train.py
-def train_model(model, train_loader, val_loader, criterion, optimizer, epochs):
-    ...
+import tensorflow as tf
+
+
+def compile_and_train(model, train_gen, val_gen, epochs=20):
+
+    model.compile(
+        optimizer=tf.keras.optimizers.Adam(learning_rate=1e-4),
+        loss="categorical_crossentropy",
+        metrics=["accuracy"]
+    )
+
+    history = model.fit(
+        train_gen,
+        validation_data=val_gen,
+        epochs=epochs
+    )
+
+    return history
